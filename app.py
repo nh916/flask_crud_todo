2eb1d210e5e1da20f3b83da45f6b44194379b77c
Navid Hariri <navihariri25@gmail.com>
2021-07-11 01:47:26 -0700

    made
    create
    
    working on read
    will add database next
    
    then update
    
    then delete

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
+app.static_folder = 'static'
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/add-to-do', methods=["GET", "POST"])
+def add_to_do():
+    if request.method == 'POST':
+        todo_task = request.form.get('todo_task')
+        print('task', todo_task)
+        return redirect(url_for('add_to_do'))
+
+    return render_template('to_do_app/create.html')
+
+
+@app.route('/view-todo')
+def view_to_do():
+    # context = {
+    #     'to_dos':
+    # }
+    return render_template('to_do_app/read.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
