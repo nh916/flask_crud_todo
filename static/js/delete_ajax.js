b662911f565f9d8732cfa993adcce4df721ab732
Navid Hariri <navihariri25@gmail.com>
2021-07-11 20:18:55 -0700

    improved code efficiency

--- static/js/delete_ajax.js
+++ static/js/delete_ajax.js
@@ -2,10 +2,8 @@ function deleteTask(taskId) {
             axios.post(`/delete-to-do/${taskId}`, {
                 'taskToDelete': taskId,
             }).then((response) => {
-                console.log(response);
                 if (response.status === 200) {
-                    let result = document.querySelectorAll(`.id_${taskId}`);
-                    result.forEach(el => el.remove());
+                    let result = document.querySelector(`#id_${taskId}`).remove();
                 }
             })
 
